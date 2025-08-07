9ae94780cda2fb3577bcfd1816878ce8e8874651
mzckawa <mzc@cin.ufpe.br>
2025-08-07 12:08:09 -0300

    First commit. Files regarding studies about markdown and classes were created. Before using sprites, I'm developing my understanding about how game collectibles work using drawn shapes on the screen. Later, I want to go back to the collaborative repository and implement the collectibles using the correct sprites and other parts of the game made by me peers.

---
+++ exploring_classes.py
@@ -0,0 +1,66 @@
+
+# today, my goal is to explore classes and make collectibles that appear randomly on the screen and move from the right side of the screen to the left
+# later, these colletibles will be represented as sprites, but, for now, I'm just going to draw some shapes on the screen and work on the fundamental logic
+import pygame 
+from pygame.locals import *
+from sys import exit
+from random import randint
+
+# defining my screen's measures
+length = 860
+width = 640 
+screen = pygame.display.set_mode((length, width))
+
+# creating the player
+player_x = 0
+player_y = 0
+
+# creating the collectible's class
+
+class Collectible:
+
+    def __init__(self, x_pos, y_pos):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+
+    def draw_collec(self):
+
+        pygame.draw.circle(screen, lilac, (self.x_pos, self.y_pos), 100)
+
+    # def movement(self):
+    #     x_pos += 1
+    #     if x_pos >= length:
+    #         x_pos = 0
+
+# naming some RGB tuples 
+black = (0, 0, 0)
+white = (255, 255, 255)
+light_green = (128, 200, 128)
+pink = (255, 0, 255)
+lilac = (200, 162, 200)
+
+while True:
+
+    # screen.fill(light_green)
+    pygame.display.flip()
+
+    collec_1 = Collectible(width//2, length//2)
+    collec_1.draw_collec()
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+        
+
+
+
+
+
+
+        
+
+
+
+
---
+++ pygame diary.md
@@ -0,0 +1,12 @@
+## Aug 7th, 2025: 
+
+Yesterday, I watched an introductory explanationa about classes, attributes and methods. Today, I'll explore how to implement this knowledge on my Pygame project. 
+
+I also learned what are sprites and how to use them in Pygame. I will definitely need some practice to stick the commands in my memory, but I have broaden my understanding of how Pygame commands are going to help me creat Pychave's collectibles.
+
+Early today, I learned how to remotely access a private repository of my GitHub account (since learning_pygame is currently private), which included learning how to creat and use SSH keys. It seemed confusing at first, but it was fascinating to finally understand the logic behind it! 
+
+Also, I learned that sometimes you need to use pip3 instead of pip to install things and explored markdown commands.
+
+
+
